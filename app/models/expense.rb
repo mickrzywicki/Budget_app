6ac89fe281df9ab30b8fe2d88e54bf584f723202
Krzyfho <krzywicki.michal1@gmail.com>
2021-04-27 11:00:00 +0200

    Create tree directory for locales en.yml, change some tests for clear message after red, add new regex for 1 or 2 digits decimal points

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,7 +1,7 @@
 class Expense < ApplicationRecord
   validates :name, :price, :paid_on, presence: true
   validates :name, length: { minimum: 2, maximum: 100 }
-  validates :price, format: { with: /\A\d+(\.\d{2})?\Z/, allow_blank: true },
+  validates :price, format: { with: /\A\d+(\.\d{1,2})?\Z/, allow_blank: true },
                     numericality: { greater_than: 0 }
   validate :paid_on_cannot_be_in_the_future
 
@@ -9,6 +9,6 @@ class Expense < ApplicationRecord
 
 
   def paid_on_cannot_be_in_the_future
-    errors.add(:date, I18n.t('mail_error_valid')) if paid_on > Date.today
+    errors.add(:paid_on, I18n.t('mail_error_valid')) if paid_on > Date.today
   end
 end
--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -11,18 +11,22 @@ RSpec.describe Expense, type: :model do
     expect(subject).to be_valid
   end
 
-  it 'is not valid without a name' do
+  it 'is not valid without a name and name are too long' do
     subject.name = nil
     expect(subject).to_not be_valid
+    expect(subject.errors.messages[:name]).to eq ["can't be blank", "is too short (minimum is 2 characters)"]
   end
 
   it 'is not valid without a price' do
     subject.price = nil
     expect(subject).to_not be_valid
+    expect(subject.errors.messages[:price]).to eq ["can't be blank","is not a number"]
   end
 
   it 'is not valid without a paid_on date' do
     subject.paid_on = Date.tomorrow
+    # expect(subject).to raise_error(I18n.t('mail_error_valid'))
     expect(subject).to_not be_valid
+    expect(subject.errors.messages[:paid_on]).to eq [I18n.t('mail_error_valid')]
   end
 end
