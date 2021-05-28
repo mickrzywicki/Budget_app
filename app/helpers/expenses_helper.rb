f5ef1a5b2a34eb233e4a423c9e8144732659db08
Michał Krzywicki <krzywicki.michal1@gmail.com>
2021-05-28 15:03:03 +0200

    Working sort columns
    In helper added possibility to improve sorting.

--- app/helpers/expenses_helper.rb
+++ app/helpers/expenses_helper.rb
@@ -1,2 +1,7 @@
 module ExpensesHelper
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    link_to title, { sort: column, direction: direction }, { class: "btn" }
+  end
 end
