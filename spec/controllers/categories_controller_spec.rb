c3841f804ee85a1fdba97c937d8aef5165db36d3
Krzyfho <krzywicki.michal1@gmail.com>
2021-06-07 10:50:31 +0200

    Working edit for categories
    - Added also index page for categories
    - Edit actions and view for categories
    - Button for delete and modal also added.

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -70,4 +70,79 @@ RSpec.describe CategoriesController, type: :controller do
       end
     end
   end
+
+  describe 'GET #edit' do
+    let(:user) { create(:user) }
+    let(:category) { create(:category) }
+
+    context 'from login user' do
+      before do
+        sign_in user
+        get :edit, params: { id: category.id }
+      end
+
+      it 'should return 200:OK' do
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'renders the edit template' do
+        expect(response).to render_template('edit')
+      end
+    end
+  end
+
+  describe 'PUT #update' do
+    let(:user) { create(:user) }
+    let(:category) { create(:category) }
+
+    context 'from login user and valid attributes' do
+      before do
+        sign_in user
+        patch :update,
+              params: {
+                id: category.id,
+                category: {
+                  name: 'Vegetables'
+                }
+              }
+      end
+
+      it 'successful redirect after PUT' do
+        expect(response).to redirect_to('/categories')
+      end
+
+      it 'create flash message' do
+        expect(flash[:success]).to eq I18n.t('flash.controller.good_update')
+      end
+
+      it 'has 302:Redirect' do
+        expect(response).to have_http_status(:redirect)
+      end
+
+      it 'save record to database with success' do
+        expect(Category.count).to eq(1)
+      end
+    end
+
+    context 'from login user and invalid attributes' do
+      before do
+        sign_in user
+        patch :update,
+              params: {
+                id: category.id,
+                category: {
+                  name: 'T'
+                }
+              }
+      end
+
+      it 'has invalid name' do
+        expect(flash[:danger]).to eq I18n.t('flash.controller.bad_update')
+      end
+
+      it 'render new template' do
+        expect(response).to render_template('categories/edit')
+      end
+    end
+  end
 end
