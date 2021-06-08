b49424cd41b613a2a95b5940d074c906500dd46a
Krzyfho <krzywicki.michal1@gmail.com>
2021-06-08 11:06:21 +0200

    Fix the controller and index for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@ class CategoriesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @categories = Category.order(:name).page params[:page]
+    @categories = Category.page(params[:page])
   end
 
   def new
